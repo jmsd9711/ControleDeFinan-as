4b13b3c4277f0ddb2ee2a0d6d45652d0a7e66ca7
João Matheus <joao.delgado.ifpr.2023@gmail.com>
2025-12-09 18:23:05 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,84 @@
+var salario = parseFloat(localStorage.getItem("salario")) || 0
+var despesas = JSON.parse(localStorage.getItem("despesas")) || []
+
+atualizarDespesasTabela()
+atualizarResumo()
+
+function registrarSalario() {
+    var campo = document.getElementById("salarioInput")
+    var valor = parseFloat(campo.value)
+
+    if (!valor || valor <= 0) {
+        alert("Digite um salário válido")
+        return
+    }
+
+    salario = valor
+    localStorage.setItem("salario", salario)
+
+    campo.value = ""
+    atualizarResumo()
+}
+
+function registrarDespesa() {
+    var data = document.getElementById("despesaData").value
+    var nome = document.getElementById("despesaNome").value
+    var valor = parseFloat(document.getElementById("despesaValor").value)
+
+    if (data === "" || nome === "" || !valor || valor <= 0) {
+        alert("Preencha todos os campos corretamente.")
+        return
+    }
+
+    var novaDespesa = { data: data, nome: nome, valor: valor }
+    despesas.push(novaDespesa)
+
+    localStorage.setItem("despesas", JSON.stringify(despesas))
+
+    document.getElementById("despesaData").value = ""
+    document.getElementById("despesaNome").value = ""
+    document.getElementById("despesaValor").value = ""
+
+    atualizarDespesasTabela()
+    atualizarResumo()
+}
+
+
+function atualizarDespesasTabela() {
+    var tabela = document.getElementById("despesasTabela")
+    tabela.innerHTML = ""
+
+    for (var i = 0; i < despesas.length; i++) {
+        var d = despesas[i]
+        
+        var tr = document.createElement("tr")
+
+        var tdData = document.createElement("td")
+        tdData.innerText = d.data
+        tr.appendChild(tdData)
+
+        var tdNome = document.createElement("td")
+        tdNome.innerText = d.nome
+        tr.appendChild(tdNome)
+
+        var tdValor = document.createElement("td")
+        tdValor.innerText = d.valor.toFixed(2)
+        tr.appendChild(tdValor)
+
+        tabela.appendChild(tr)
+    }
+}
+
+function atualizarResumo() {
+    var total = 0
+    for (var i = 0; i < despesas.length; i++) {
+        total += despesas[i].valor
+    }
+    var saldo = salario - total
+    document.getElementById("salarioValor").textContent = salario.toFixed(2)
+    document.getElementById("despesasTotal").textContent = total.toFixed(2)
+    document.getElementById("saldoFinal").textContent = saldo.toFixed(2)
+}
+
+
+
